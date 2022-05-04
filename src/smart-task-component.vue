c103695dcbdc47bd90f69dbadd4347d2fe01e55b
Enrico Willemse <enricowillemse.was@gmail.com>
2022-05-04 03:27:50 +0000

    Update

--- src/smart-task-component.vue
+++ src/smart-task-component.vue
@@ -23,17 +23,82 @@ export default /*#__PURE__*/ defineComponent({
 });
 </script>
 
+
 <template>
   <form>
-    <div class="my-5">
-      <button
-        type="button"
-        @click="submitForm"
-        class="btn btn-sm btn-primary"
-      >
-       Mark as in Progress
-      </button>
+    <div class="d-flex flex-row bd-highlight mb-3">
+      <div class="mx-4">
+        <div class="mb-3">
+          <img
+            width="100px"
+            src="https://res.cloudinary.com/dbwpvgyrn/image/upload/c_scale,w_300/v1643297464/myworkflows/HackerRank_logo.png"
+          />
+        </div>
+      </div>
+      <div class="w-100">
+        <div class="mb-3">
+          <label class="form-label">Candidate Email Address</label>
+          <input
+            type="email"
+            placeholder="email@myworkflows.io"
+            class="form-control"
+          />
+          <div class="form-text">
+            Specify the email address for the user completing the assignment.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Due By Date</label>
+          <input type="datetime-local" class="form-control" />
+          <div class="form-text">
+            Specify the date when the assignment is due by.
+          </div>
+        </div>
+
+        <div class="mb-4">
+          <h4 class="form-label">Assignments</h4>
+          <div class="my-2 py-1 row">
+            <div class="col-md-6">
+            <div class="me-1 card">
+              <div class="card-body">
+                <h5>
+                  Top Competitors <span class="badge bg-warning">Medium</span>
+                </h5>
+                <ul>
+                  <li>SQL (Intermediate)</li>
+                  <li>Max Score: <span class="fw-bold">30</span></li>
+                  <li>
+                    Success Rate: <span class="badge bg-success">96.16%</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+              </div>
+          <div class="col-md-6">
+            <div class="card">
+              <div class="card-body">
+                <h5>
+                  Cards Permutation
+                  <span class="badge bg-danger">Expert</span>
+                </h5>
+                <ul>
+                  <li>Algorithms</li>
+                  <li>Max Score: <span class="fw-bold">85</span></li>
+                  <li>
+                    Success Rate: <span class="badge bg-success">60.35%</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <button type="submit" class="float-end btn btn-success">
+      Email Assignment
+    </button>
   </form>
 </template>
 
